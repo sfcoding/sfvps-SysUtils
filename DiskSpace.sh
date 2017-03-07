458490dbf6b122de5215c9a2d60f621c51331d42
root <root@SFvps>
2017-03-07 13:55:10 +0100

    typo

--- DiskSpace.sh
+++ DiskSpace.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-df -hl | grep '/dev/vda1' | awk '{size+=$2;available+=$4;percent+=$5;} END{print "TotalSize: ",size"GB","\nAvailable: ",available"GB","\nUsage(%): ",percent"GB"}' | column -t 
+df -hl | grep '/dev/vda1' | awk '{size+=$2;available+=$4;percent+=$5;} END{print "TotalSize: ",size"GB","\nAvailable: ",available"GB","\nUsage(%): ",percent"%"}' | column -t 
